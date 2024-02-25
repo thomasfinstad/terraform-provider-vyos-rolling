c3b36e5c7e762f1878ee396d5c15564b27db4cae
Thomas Finstad Larsen <thomas.finstad.larsen@gmail.com>
2024-02-25 11:35:10 +0000

    test: add test for HasChild helper

--- internal/terraform/tests/firewall-name_test.go
+++ internal/terraform/tests/firewall-name_test.go
@@ -26,6 +26,10 @@ func TestFirewallIPvfourNameMarshalVyos(t *testing.T) {
 	}
 
 	got, err := helpers.MarshalVyos(context.Background(), model)
+	if err != nil {
+		t.Fatalf(`desired value can not be marshalled: %v`, err)
+	}
+
 	diff := deep.Equal(got, want)
 	if diff != nil {
 		t.Errorf("compare failed: %v, %v", diff, err)
@@ -46,6 +50,65 @@ func TestFirewallIPvfourNameUnmarshalVyos(t *testing.T) {
 	got := &resourcemodel.FirewallIPvfourName{}
 
 	err := helpers.UnmarshalVyos(context.Background(), has, got)
+	if err != nil {
+		t.Fatalf(`desired value can not be unmarshaled: %v`, err)
+	}
+
+	diff := deep.Equal(got, want)
+	if diff != nil {
+		t.Errorf("compare failed: %v, %v", diff, err)
+	}
+}
+
+// TestFirewallIPvfourNameUnmarshalVyosHasChild does some simple unmarshalling tests
+func TestFirewallIPvfourNameUnmarshalVyosHasChild(t *testing.T) {
+	has := map[string]interface{}{
+		"default-action": "drop",
+		"rule": map[string]interface{}{
+			"test-rule": map[string]interface{}{
+				"action":                "accept",
+				"disable":               map[string]interface{}{},
+				"queue":                 float64(28),
+				"packet-length-exclude": []string{"420", "13-37"},
+				"destination": map[string]interface{}{
+					"address": "127.0.0.2",
+				},
+			},
+		},
+	}
+
+	want := true
+
+	resource := &resourcemodel.FirewallIPvfourName{}
+	err := helpers.UnmarshalVyos(context.Background(), has, resource)
+	if err != nil {
+		t.Fatalf(`desired value can not be unmarshaled: %v`, err)
+	}
+
+	got := helpers.HasChild(context.Background(), resource)
+
+	diff := deep.Equal(got, want)
+	if diff != nil {
+		t.Errorf("compare failed: %v, %v", diff, err)
+	}
+}
+
+// TestFirewallIPvfourNameUnmarshalVyosNoChild does some simple unmarshalling tests
+func TestFirewallIPvfourNameUnmarshalVyosNoChild(t *testing.T) {
+	has := map[string]interface{}{
+		"default-action": "drop",
+	}
+
+	want := false
+
+	resource := &resourcemodel.FirewallIPvfourName{}
+	err := helpers.UnmarshalVyos(context.Background(), has, resource)
+	if err != nil {
+		t.Fatalf(`desired value can not be unmarshaled: %v`, err)
+	}
+
+	got := helpers.HasChild(context.Background(), resource)
+
 	diff := deep.Equal(got, want)
 	if diff != nil {
 		t.Errorf("compare failed: %v, %v", diff, err)
