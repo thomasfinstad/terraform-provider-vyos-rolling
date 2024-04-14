36249ac306d466c74581e0d203126f03edb18e99
Thomas Finstad Larsen <thomas.finstad.larsen@gmail.com>
2024-04-14 14:13:40 +0000

    docs: begining of changelog generation

--- tools/generate-changelog/main.go
+++ tools/generate-changelog/main.go
@@ -9,6 +9,10 @@ import (
 	"slices"
 	"strings"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/hashicorp/go-version"
 	"github.com/qri-io/deepdiff"
 )
 
@@ -22,6 +26,46 @@ import (
 //  - [ ] Include resource changes in calculation for release version bumping
 
 func main() {
+	repo, err := git.PlainOpen("../..")
+	die(err)
+
+	previousVersion, err := version.NewVersion("0.0.0")
+	die(err)
+
+	itr, err := repo.Tags()
+	die(err)
+	err = itr.ForEach(func(ref *plumbing.Reference) error {
+		fmt.Println("Comparing versions: ", previousVersion.Original(), ref.Name().Short())
+		v, err := version.NewVersion(ref.Name().Short())
+		die(err)
+		if v.GreaterThan(previousVersion) {
+			previousVersion = v
+		}
+		return nil
+	})
+	die(err)
+
+	fmt.Println("Previous revision: ", previousVersion.Original())
+	hash, err := repo.ResolveRevision(plumbing.Revision(previousVersion.Original()))
+	die(err)
+	releaseCommit, err := repo.CommitObject(*hash)
+	die(err)
+
+	commits, err := repo.Log(&git.LogOptions{
+		Since: &releaseCommit.Author.When,
+	})
+	die(err)
+
+	_, err = commits.Next()
+	die(err)
+	err = commits.ForEach(func(commit *object.Commit) error {
+		fmt.Println(commit.Message)
+		return nil
+	})
+	die(err)
+}
+
+func main2() {
 	var err error
 	//args := os.Args[1:]
 	// args := []string{
